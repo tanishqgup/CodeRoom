899766f96c5f5f4de54c649897b9683e0ecd4672
tanishqgup <gtanishq96@gmail.com>
2021-11-22 23:41:29 +0530

    fake

--- client/room.js
+++ client/room.js
@@ -1,7 +1,7 @@
-const socket = io("http://localhost:3000", {
-    transports: ["websocket"],
-});
-// const socket = io();
+// const socket = io("http://localhost:3000", {
+//     transports: ["websocket"],
+// });
+const socket = io("/");
 
 const today = new Date();
 
