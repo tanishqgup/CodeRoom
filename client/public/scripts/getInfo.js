6750b05e1ea51076e56a5d9dab7ad6d7b928d122
tanishqgup <gtanishq96@gmail.com>
2021-11-29 16:34:14 +0530

    added links for linked in profiles and added features to redirect to editors page on clicking enter key after filling details

--- client/public/scripts/getInfo.js
+++ client/public/scripts/getInfo.js
@@ -1,8 +1,16 @@
 function redirectToEditor() {
     // window.location.href = ROOM_IDD + "/" + document.getElementById("name").value;
+    const nameValue = document.getElementById("name").value;
+    if(nameValue === "") return;
     if(sessionStorage.getItem("name")) {
         sessionStorage.removeItem("name");
     }
-    sessionStorage.setItem("name", document.getElementById("name").value);
+    sessionStorage.setItem("name", nameValue);
     window.location.href = ROOM_IDD;
 }
+
+document.getElementById("name").addEventListener('keydown' , (e) => {
+    if(e.keyCode === 13) {
+        document.getElementById("enter-room").click();
+    }
+});
--- client/public/scripts/joinRoom.js
+++ client/public/scripts/joinRoom.js
@@ -6,3 +6,15 @@ function redirectToEditor() {
     sessionStorage.setItem("name", document.getElementById("name").value);
     window.location.href = "/" + roomid;
 }
+
+document.getElementById("roomid").addEventListener('keydown', (e) => {
+    if(e.keyCode === 13) {
+        document.getElementById("name").focus();
+    }
+})
+
+document.getElementById("name").addEventListener('keydown' , (e) => {
+    if(e.keyCode === 13) {
+        document.getElementById("enter-room").click();
+    }
+});
